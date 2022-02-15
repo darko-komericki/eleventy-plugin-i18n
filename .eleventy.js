235e30d406b02ea5e6270c7f278d70981dad5d29
Darko Komerički <darkomericki@gmail.com>
2022-02-15 18:30:51 +0100

    modified path to page property to support 11ty 1.0.0

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,6 @@ module.exports = function (eleventyConfig, configGlobalOptions = {}) {
 
   eleventyConfig.addFilter('i18n', function (key, data, localeOverride) {
     // TBC Here we need to determine if filter is being used on page or in include, respectively
-    const page = this.page || this.ctx.page;
-    return i18n(key, data, localeOverride, pluginOptions, page);
+    const page = this.page || this.context.environments.page;    return i18n(key, data, localeOverride, pluginOptions, page);
   });
 };
